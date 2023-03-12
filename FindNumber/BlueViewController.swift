495b5f7bd328459ce1807d9f832f3d799d242988
Nursat2022 <nursatsakyshev2003@gmail.com>
2023-03-12 19:45:14 +0600

    FirstGame

--- FindNumber/BlueViewController.swift
+++ FindNumber/BlueViewController.swift
@@ -14,6 +14,7 @@ class BlueViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         testLabel.text = textForLabel
+        //something
     }
     @IBAction func goToGreenControlller(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
